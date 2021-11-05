29a644baef54416519c2ee93fb64888942204699
Reon0429-cat <oonishireon110@gmail.com>
2021-11-05 10:31:28 +0900

    /api/v1/listにGETメソッドでアクセスしてきたときにTODOリストをJSONで返す

---
+++ todo.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+// http://localhost:3000/api/v1/list にアクセスしてきたときにTODOリストを返す
+app.get('/api/v1/list', (req, res) => {
+    const todoList = [
+      {title: 'JavaScriptを勉強する', done: true},
+      {title: 'Node.jsを勉強する', done: false},
+      {title: 'Web APIを作る', done: false}
+    ];
+    res.json(todoList);
+});
+
+app.listen(3000, () => console.log('Listening on port 3000'));
