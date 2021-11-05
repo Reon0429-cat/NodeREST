a98ab4f84c42459bf0054296950392572bb8ae84
Reon0429-cat <oonishireon110@gmail.com>
2021-11-05 09:05:22 +0900

    get, post, put, delete練習

--- index.js
+++ index.js
@@ -1,16 +1,62 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
 const courses = [
-  {id: 1, name: "国語"},
-  {id: 2, name: "英語"},
-  {id: 3, name: "理科"},
-  {id: 4, name: "数学"},
-  {id: 5, name: "社会"},
+    {
+        id: 1,
+        name: "国語"
+    }, {
+        id: 2,
+        name: "英語"
+    }, {
+        id: 3,
+        name: "理科"
+    }, {
+        id: 4,
+        name: "数学"
+    }, {
+        id: 5,
+        name: "社会"
+    }
 ];
 
 app.get('/api/courses', (request, response) => {
-  response.send(courses);
+    response.send(courses);
 });
+
+app.post('/api/courses', (request, response) => {
+    const course = {
+        id: courses.length + 1,
+        name: request.body.name
+    };
+    courses.push(course);
+    response.send(course);
+});
+
+app.put('/api/courses/:id', (request, response) => {
+    const course = courses.find(c => c.id === parseInt(request.params.id));
+    if (!course) {
+        return response
+            .status(404)
+            .send('The course with the given ID was not found.');
+    }
+    course.name = request.body.name;
+    response.send(course);
+})
+
+app.delete('/api/courses/:id', (request, response) => {
+    const course = courses.find(c => c.id === parseInt(request.params.id));
+    if (!course) {
+        return response
+            .status(404)
+            .send('The course with the given ID was not found.');
+    }
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+    response.send(course);
+})
 
 // サイトのPORTを指定
 const port = process.env.PORT || 3000;
